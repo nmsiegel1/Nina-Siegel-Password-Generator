ea55a6f866afd1d89993225a27894e6126b32cc9
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-05-06 11:38:41 -0400

    input password into winodw

--- Develop/script.js
+++ Develop/script.js
@@ -3,25 +3,28 @@ var generateBtn = document.querySelector("#generate");
 // Arrays for possible passowrd characters
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var charactersArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "=", "~", "`", "?", "<", ",", ".", ">", "/", "|", ";", "'", "{", "}"];
 
 // empty arrays
 
 var resultArray = [];
 var userArray = [];
+var passwordLengthArray = [];
 
   var writePassword = function () {
-    passwordLength = parseInt(passwordLength);
+    // writePassword.reset();
     var passwordLength = window.prompt("How many characters would you like your password to be? Pick a number between 8 and 128.");
+    passwordLength = parseInt(passwordLength);
     console.log(passwordLength);
 
-    // return passwordLength;
     if(passwordLength < 8 || passwordLength > 128) {
       window.alert("Your password must be at least 8 and no more than 128 characters. Please try again.");
       writePassword();
     }else if (passwordLength >= 8 || passwordLength >= 128) {
-      console.log(">>" + passwordLength);
+      passwordLengthArray = passwordLengthArray.concat(passwordLength);
+      console.log(">>" + passwordLengthArray);
+      // return passwordLength;
       characterSelections();
     }else {
       window.alert("You must enter your desired password length. Please try again.");
@@ -35,9 +38,7 @@ var characterSelections = function() {
     resultArray = resultArray.concat(uppercaseArray);
     console.log(resultArray);
   }
-  // else {
-    
-  // }
+
   var promptLowercase = window.confirm ("Would you like lowercase letters in your password?");
   if (promptLowercase) {
     resultArray = resultArray.concat(lowercaseArray);
@@ -45,7 +46,7 @@ var characterSelections = function() {
   }
 
   var promptNumbers = window.confirm ("Would you like numbers in your password?");
-  if (promptNumbers){
+  if (promptNumbers) {
     resultArray = resultArray.concat(numArray);
     console.log("!!" + resultArray);
   }
@@ -55,11 +56,24 @@ var characterSelections = function() {
     resultArray = resultArray.concat(charactersArray);
   console.log("??" + resultArray);
   }
+  // // else (resultArray = null) {
+  //   characterSelections();
+  
+  generatePassword();
+};
+
+var generatePassword = function() {
+  var passwordLength = passwordLengthArray[0];
+  for (var i=0; i < passwordLength; i++) {
+    userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
+    console.log(userArray);
+  }
+  inputPassword();
 };
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function inputPassword() {
+  var password = userArray.join("");
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
